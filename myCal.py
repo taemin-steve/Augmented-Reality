72683cede4d9b8f33a13bc87880fd8610f05812f
EHmin <okchrisok@naver.com>
2023-04-11 11:03:32 +0900

    try camera cal

--- myCal.py
+++ myCal.py
@@ -2,78 +2,27 @@ import cv2 as cv
 import numpy as np
 
 
-# import glob
-# image_names = glob.glob('./camera images/*.jpg')
-
-
-# imgIndex = 0
-# for fn in image_names:
-#     print(fn)
-#     img = cv.imread(fn)
-#     resized_img = cv.resize(img, (900, 1200))
-#     cv.imwrite('./resized images/image{}.jpg'.format(imgIndex), resized_img)
-#     imgIndex += 1
-
-# patternSize = (8, 6)
-
-# def detect_2d_points_from_cbimg(file_name):
-#     print("processing", file_name)
-#     img = cv.imread(file_name)
-#     h, w = img.shape[:2]
-
-#     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#     found, corners = cv.findChessboardCorners(img_gray, patternSize)
-#     if found:
-#         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_COUNT, 30, 0.1)
-#         #print(corners[10])
-#         corners = cv.cornerSubPix(img_gray, corners, (5, 5), (-1, -1), criteria)
-#         #print(corners[10])
-#         cv.drawChessboardCorners(img, patternSize, corners, found)
-#     else : # not found
-#         print('chessboard not found')
-#         return None
-    
-#     return corners, img
-
-# imgInit = cv.imread('./resized images/image0.jpg')
-# h, w = imgInit.shape[:2]
-
-# pattern_points = np.zeros((patternSize[0] * patternSize[1], 3), np.float32)
-# pattern_points[:, :2] = np.indices(patternSize).T.reshape(-1, 2)
-# pattern_points *= 10.0 # my chessboard size is 37 mm
-pattern_points = [[0.210745,0.211728,-0.193256],[0.180948,0.101878,0.087774],[0.173486,0.170723,0.082539],[0.190451,0.200691,-0.123108],[0.226842,0.121402,-0.206477]]
+pattern_points = np.array([[0.210745,0.211728,-0.193256],[0.180948,0.101878,0.087774],[0.173486,0.170723,0.082539],[0.190451,0.200691,-0.123108],[0.226842,0.121402,-0.206477]]).astype(np.float32)
 print(pattern_points)
 
-# import glob
-# image_names = glob.glob('./resized images/*.jpg')
+
 points3Ds = []
 points2Ds = []
 
-i = 0
-for cb_fileName in image_names:
-    if(detect_2d_points_from_cbimg(cb_fileName)):
-        corners, img = detect_2d_points_from_cbimg(cb_fileName)
-        cv.imshow("test" + str(i), img)
-        points3Ds.append(pattern_points) # 3D좌표
-        points2Ds.append(corners) # 2D 좌표
+img_cr = cv.imread('C:/AR/Augmented-Reality/1.jpg', cv.IMREAD_GRAYSCALE)
+h,w = img_cr.shape[:2]
+
+points3Ds.append(pattern_points)
+points2Ds.append(np.array([[335, 315],[1169, 1101],[613, 1130],[404, 798],[1014, 229]]).astype(np.float32))
     
 print(points3Ds)
 print(points2Ds)
-#cv.imshow("test", img)
-#cv.imwrite("./testout2.jpg", img)
-#cv.waitKey(0)
-#print(pattern_points)
-#print(pattern_points)
-rms_err, intrisic_mtx, dist_coefs, rvecs, tvecs = cv.calibrateCamera(points3Ds, points2Ds, (w, h), None, None)   
+
+rms_err, intrisic_mtx, dist_coefs, rvecs, tvecs = cv.calibrateCamera(points3Ds, points2Ds, (w, h), None, None)  
+#! For non-planar calibration rigs the initial intrinsic matrix must be specified in function 'cvCalibrateCamera2Internal'
 
 print("\nRMS:", rms_err)
 print("camera intrinsic matrix:\n", intrisic_mtx)
 print("distortion coefficients: ", dist_coefs.ravel())
 
-# undistort
-img = imgInit
-dst = cv.undistort(img, intrisic_mtx, dist_coefs, None, intrisic_mtx)
-cv.imshow("dist", img)
-cv.imshow("undist", dst)
-cv.waitKey(0)
     
